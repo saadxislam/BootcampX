1f42d2f2e112ffda250fefadc3e34e240b930484
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-10-13 02:01:56 +0000

    testting

--- 2_queries_joins/2_2_cohortsTotalAssignmentDuration.sql
+++ 2_queries_joins/2_2_cohortsTotalAssignmentDuration.sql
@@ -3,3 +3,10 @@ FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
+
+
+SELECT students.name, count(assignment_submissions.*)
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+HAVING COUNT(assignment_submissions.*) < 100
+GROUP BY student.name;
