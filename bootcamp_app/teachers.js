417e7f162cb0ed280a841b0e8a020390ea4cc8d9
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-10-15 03:00:41 +0000

    changed to parameterized values

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,8 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2]
+const value = [ cohortName ];
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -14,10 +16,11 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, value)
 .then(res => {
+// console.log('res :', res);
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
